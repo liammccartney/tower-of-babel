281e8c250e7407f6e5211907255ecdbada687dca
Liam McCartney <liam.mccartney@gmail.com>
2016-10-03 13:45:56 -0400

    Add solutions for challenges 6,7,8

---
+++ 8_iterator_for_of/solution.js
@@ -0,0 +1,30 @@
+const max = +process.argv[2];
+
+let FizzBuzz = {
+    [Symbol.iterator]() {
+        let num = 1;
+        return {
+            next() {
+                if (num >= max) { 
+                    return {done: true};
+                }
+
+                let value = num;
+
+                if (value % 15 === 0) {
+                    value = 'FizzBuzz';
+                } else if (value % 5 === 0) {
+                    value = 'Buzz';
+                } else if (value % 3 === 0) {
+                    value = 'Fizz';
+                }
+                num++
+                return {done: false, value: value}
+            }
+        }
+    }
+}
+
+for (var n of FizzBuzz) {
+    console.log(n);
+}
